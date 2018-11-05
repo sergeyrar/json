172500198a774aa6457c9571f4a752a7a8557377
root <root@ubuntu.ubuntu-domain>
2018-11-05 13:25:44 +0200

    Successfully parsed one field

--- 1.c
+++ 1.c
@@ -10,16 +10,22 @@ int main()
 	json_error_t j_error;
 	char buf[2000];
 	int size;		
+	int hb;
 
 	fp = fopen("./json.txt", "r");
 	
-	while (fscanf(fp, "%d", &size) != EOF)
+	while (fscanf(fp, "%d\n", &size) != EOF)
 	{	
-//		fread(buf, 1, size, fp);
-		if (( json_obj = json_loadf(fp, 0, &j_error)) == NULL)
+		fread(buf, 1, size, fp);
+		if (( json_obj = json_loadb(buf, size, 0, &j_error)) == NULL)
 		{
-			printf("Error: %s", j_error.text);	
+			printf("line: %d, column: %d, position: %d, source: %s, Error: %s"
+				,j_error.line, j_error.column, j_error.position, j_error.source, j_error.text);	
 		}
+		
+		json_unpack(json_obj, "{s:i}", "heartbeat", &hb);
+		
+		printf("Heartbeat = %d\n", hb);
 	}
 	
 	return 0; 
